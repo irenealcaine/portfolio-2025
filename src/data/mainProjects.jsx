fd3db2c94b0567949d3c0dd5d9dff53bc8634aec
irenealcaine <101673351+irenealcaine@users.noreply.github.com>
2025-12-01 15:52:42 +0000

    update main projects

--- src/data/mainProjects.jsx
+++ src/data/mainProjects.jsx
@@ -1,14 +1,14 @@
-import list from "../assets/list.png"
+import cv from "../assets/cv.png"
 import spiral from "../assets/spiral.svg"
 import resources from "../assets/code.svg"
 
 import react from "../assets/react.svg"
+import vue from "../assets/vue.svg"
+import tailwind from "../assets/tailwind.svg"
 
 import heroui from "../assets/hero-ui.png"
-import sass from "../assets/sass.png"
 
 import supabase from "../assets/supabase.svg"
-import firebase from "../assets/firebase.svg"
 
 export const mainProjects = [
   {
@@ -27,9 +27,9 @@ export const mainProjects = [
   },
   {
     id: "project3",
-    icon: list,
-    tech: [react, sass, firebase],
+    icon: cv,
+    tech: [vue, tailwind],
     color: "light-blue",
-    href: "https://lista-compra-irene.netlify.app/"
+    href: "https://curriculum-vitae-builder-cv.netlify.app/"
   },
 ]
